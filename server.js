ca83f02ea80079f0267e8301c8f2095dffc8ef63
Jinto Varghese <jintov@mindtree.com>
2017-06-12 13:47:39 -0400

    added content type for 1 operation

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/products', function(req, res) {
 		
 		var response = JSON.stringify(result);
 		res.setHeader('Content-Length', response.length);
+		res.setHeader('Content-Type', "application/json");
 		res.end(response);
 	});
 })
